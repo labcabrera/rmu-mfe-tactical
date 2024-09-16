d81d2eed60f5440287d7dad0f1a330ec29794844
labcabrera <lab.cabrera@gmail.com>
2024-09-16 14:34:54 +0200

    Minor excepcion handling.

--- src/components/TacticalGameList.jsx
+++ src/components/TacticalGameList.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+import CloseIcon from '@mui/icons-material/Close';
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
 import List from '@mui/material/List';
+import Snackbar from '@mui/material/Snackbar';
 import Stack from '@mui/material/Stack';
 
 import TacticalGameListItem from "./TacticalGameListItem";
@@ -13,17 +16,29 @@ const TacticalGameList = () => {
     const navigate = useNavigate();
     const [games, setGames] = useState([]);
 
+    const [displayError, setDisplayError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+
     const getGames = async () => {
-        const response = await fetch("http://localhost:3001/v1/tactical-games", {
-            method: "GET",
-        });
-        const data = await response.json();
-        setGames(data);
+        try {
+            const response = await fetch("http://localhost:3001/v1/tactical-games", {
+                method: "GET",
+            });
+            const data = await response.json();
+            setGames(data);
+        } catch (error) {
+            setDisplayError(true);
+            setErrorMessage(error.message);
+        }
     };
 
     const createNewGame = async () => {
         navigate("/tactical/creation");
-    }
+    };
+
+    const handleSnackbarClose = () => {
+        setDisplayError(false);
+    };
 
     useEffect(() => {
         getGames();
@@ -47,6 +62,23 @@ const TacticalGameList = () => {
                     ))}
                 </List>
             </div>
+            <Snackbar
+                open={displayError}
+                anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                onClose={handleSnackbarClose}
+                message={errorMessage}
+                action={
+                    <React.Fragment>
+                        <IconButton
+                            aria-label="close"
+                            color="inherit"
+                            sx={{ p: 0.5 }}
+                            onClick={handleSnackbarClose}>
+                            <CloseIcon />
+                        </IconButton>
+                    </React.Fragment>
+                }
+            />
         </div>
     );
 }
