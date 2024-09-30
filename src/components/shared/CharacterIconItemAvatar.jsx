a914faad6ab47c80b94d2686ae2a0827f740f300
labcabrera <lab.cabrera@gmail.com>
2024-09-30 19:21:01 +0200

    Update avatar image resolution.

--- src/components/shared/CharacterIconItemAvatar.jsx
+++ src/components/shared/CharacterIconItemAvatar.jsx
@@ -3,25 +3,11 @@ import React from "react";
 import Avatar from '@mui/material/Avatar';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 
-import caveTrollIcon from '../../assets/images/races/lotr-cave-troll.jpg';
-import gondorIcon from '../../assets/images/races/lotr-gondor.jpg';
-import orcIcon from '../../assets/images/races/lotr-orc.jpg';
-import witchKingIcon from '../../assets/images/races/lotr-witch-king.jpg';
-
 const CharacterListItemAvatar = ({ character }) => {
-
-    const resolveAvatarImage = () => {
-        switch (character.info.race) {
-            case 'lotr-human': return gondorIcon;
-            case 'lotr-orc': return orcIcon;
-            case 'lotr-troll': return caveTrollIcon;
-            default: return witchKingIcon;
-        }
-    };
 
     return (
         <ListItemAvatar>
-            <Avatar src={resolveAvatarImage()} />
+            <Avatar src={`/static/images/races/${character.info.race}.jpg`} />
         </ListItemAvatar>
     );
 }
