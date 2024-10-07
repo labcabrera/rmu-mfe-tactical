f8f3a35c0a9ad3770ce752bdf03b941acec25468
labcabrera <lab.cabrera@gmail.com>
2024-10-07 17:48:27 +0200

    Minor style.

--- src/components/shared/ActionPointSelector.jsx
+++ src/components/shared/ActionPointSelector.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import Rating from '@mui/material/Rating';
 import Typography from '@mui/material/Typography';
+import AdjustIcon from '@mui/icons-material/Adjust';
 
 const ActionPointSelector = ({ value, onChange }) => {
 
@@ -18,6 +19,8 @@ const ActionPointSelector = ({ value, onChange }) => {
                 defaultValue={2}
                 max={4}
                 size="large"
+                icon={<AdjustIcon fontSize="inherit" />}
+                emptyIcon={<AdjustIcon fontSize="inherit" />}
                 onChange={handleChange} />
         </>
     );
