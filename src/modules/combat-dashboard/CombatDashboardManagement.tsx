de0d75d49ad279ade69edc518b53f313226ab40e
labcabrera <lab.cabrera@gmail.com>
2025-09-07 00:51:06 +0200

    Updated combat dashboard management console.

--- src/modules/combat-dashboard/CombatDashboardManagement.tsx
+++ src/modules/combat-dashboard/CombatDashboardManagement.tsx
@@ -2,18 +2,30 @@ import React, { FC, useContext, useState } from 'react';
 import { Button, Grid } from '@mui/material';
 import { CombatContext } from '../../CombatContext';
 import { useError } from '../../ErrorContext';
-import { addActorRoundHp } from '../api/actor-rounds';
+import { ActorRoundEffect, addActorRoundHp, addActorRoundEffect } from '../api/actor-rounds';
 import NumericTextField from '../shared/inputs/NumericTextField';
 import SelectActorRound from '../shared/selects/SelectActorRound';
+import SelectEffect from '../shared/selects/SelectEffect';
 
 const CombatDashboardManagement: FC = () => {
   const { showError } = useError();
   const { actorRounds, updateActorRound } = useContext(CombatContext);
-  const [hpActorId, setHpActorId] = useState<string | null>(null);
+  const [actorId, setActorId] = useState<string | null>(null);
   const [hpValue, setHpValue] = useState<string | null>(null);
+  const [effectFormData, setEffectFormData] = useState<ActorRoundEffect | null>(null);
 
   const handleAddHp = () => {
-    addActorRoundHp(hpActorId!, parseInt(hpValue!))
+    addActorRoundHp(actorId!, parseInt(hpValue!))
+      .then((data) => {
+        updateActorRound(data);
+      })
+      .catch((err) => {
+        showError(err.message);
+      });
+  };
+
+  const handleAddEffect = () => {
+    addActorRoundEffect(actorId!, effectFormData)
       .then((data) => {
         updateActorRound(data);
       })
@@ -27,15 +39,25 @@ const CombatDashboardManagement: FC = () => {
   return (
     <Grid container spacing={2}>
       <Grid size={1}>
-        <SelectActorRound value={hpActorId} actorRounds={actorRounds} onChange={(e) => setHpActorId(e.id)} />
+        <SelectActorRound value={actorId} actorRounds={actorRounds} onChange={(e) => setActorId(e.id)} />
+      </Grid>
+      <Grid size={1}>
+        <Button variant="contained" disabled={!actorId || !hpValue} onClick={handleAddHp}>
+          Add HP
+        </Button>
       </Grid>
       <Grid size={1}>
         <NumericTextField label="HP" value={hpValue} onChange={(e) => setHpValue(e.target.value)} />
       </Grid>
+      <Grid size={12}></Grid>
+      <Grid size={1}></Grid>
       <Grid size={1}>
-        <Button variant="contained" disabled={!hpActorId || !hpValue} onClick={handleAddHp}>
-          Add HP
+        <Button variant="contained" disabled={!actorId} onClick={handleAddEffect}>
+          Add Effect
         </Button>
+      </Grid>
+      <Grid size={1}>
+        <SelectEffect value={effectFormData?.status || ''} onChange={(e) => setEffectFormData({ ...effectFormData, status: e.target.value })} />
       </Grid>
     </Grid>
   );
---
+++ src/modules/shared/selects/SelectEffect.tsx
@@ -0,0 +1,35 @@
+import React, { ChangeEvent, FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import MenuItem from '@mui/material/MenuItem';
+import TextField from '@mui/material/TextField';
+
+type SelectEffectProps = {
+  value: string;
+  name?: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+};
+
+const SelectEffect: FC<SelectEffectProps> = ({ value, name = 'effect', onChange }) => {
+  const { t } = useTranslation();
+  const options = ['death', 'bleeding', 'stunned', 'blinded', 'prone'];
+
+  return (
+    <TextField
+      select
+      label={t('effect')}
+      name={name}
+      value={value === undefined || value === null || options.length === 0 ? '' : value}
+      fullWidth
+      variant="standard"
+      onChange={onChange}
+    >
+      {options.map((option, index) => (
+        <MenuItem key={index} value={option}>
+          {t(`effect-${option}`)}
+        </MenuItem>
+      ))}
+    </TextField>
+  );
+};
+
+export default SelectEffect;
