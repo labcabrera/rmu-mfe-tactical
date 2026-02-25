e40db27a89c0ace7e301572f3f048e7d2ad41daa
labcabrera <lab.cabrera@gmail.com>
2026-02-25 14:09:29 +0100

    Update image url.

--- src/modules/shared/avatars/TacticalGameAvatar.tsx
+++ src/modules/shared/avatars/TacticalGameAvatar.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import Avatar from '@mui/material/Avatar';
-import { TacticalGame } from '../../api/tactical-game';
+import { TacticalGame } from '../../api/tactical-game.dto';
+
+const imageBaseUrl = process.env.RMU_MFE_ASSETS!;
+const defaultImage = `${imageBaseUrl}images/avatars/avatar-017.png`;
 
 const TacticalGameAvatar: React.FC<{
   tacticalGame: TacticalGame;
   size?: number;
 }> = ({ tacticalGame, size = 70 }) => {
-  const defaultImage = '/static/images/avatars/avatar-017.png';
-
-  const resolveImage = (game: TacticalGame) => {
-    if (game.imageUrl) return game.imageUrl;
-    return defaultImage;
-  };
-
-  return <Avatar src={resolveImage(tacticalGame)} sx={{ width: size, height: size }} />;
+  return (
+    <Avatar src={tacticalGame.imageUrl ? tacticalGame.imageUrl : defaultImage} sx={{ width: size, height: size }} />
+  );
 };
 
 export default TacticalGameAvatar;
