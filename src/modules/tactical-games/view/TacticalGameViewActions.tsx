3ef75142641ca185e4600d8f9a626c187b67f064
labcabrera <lab.cabrera@gmail.com>
2026-03-31 14:15:55 +0200

    Shared lib refactor.

--- src/modules/tactical-games/view/TacticalGameViewActions.tsx
+++ src/modules/tactical-games/view/TacticalGameViewActions.tsx
@@ -1,13 +1,17 @@
 import React, { Dispatch, FC, SetStateAction, useState } from 'react';
 import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import { Box, Breadcrumbs, Link, Stack } from '@mui/material';
-import { CancelButton, DeleteDialog, RefreshButton } from '@labcabrera-rmu/rmu-react-shared-lib';
+import {
+  CancelButton,
+  DeleteButton,
+  DeleteDialog,
+  EditButton,
+  RefreshButton,
+} from '@labcabrera-rmu/rmu-react-shared-lib';
 import { t } from 'i18next';
 import { useError } from '../../../ErrorContext';
 import { deleteTacticalGame, fetchTacticalGame, startRound } from '../../api/tactical-game';
 import { TacticalGame } from '../../api/tactical-game.dto';
-import DeleteButton from '../../shared/buttons/DeleteButton';
-import EditButton from '../../shared/buttons/EditButton';
 import PlayButton from '../../shared/buttons/PlayButton';
 
 const TacticalGameViewActions: FC<{
